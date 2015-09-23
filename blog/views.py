07aba3d2b4512a1535ee13ee7f486b9001724eb8
phablo100 <loermia_iela@hotmail.com>
2015-09-23 20:16:08 -0300

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, Http404
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from .models import Pergunta
 
 def index(request):
     return render(request, "index.html", {});
+
+def detail(request, pergunta_id):
+    try:
+        pergunta = Pergunta.objects.get(pk=pergunta_id)
+    except Pergunta.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'pergunta': pergunta})
+def vote(request, pergunta_id):
+	return HttpResponse("Você está votando na pergunta %s" %pergunta_id)	
+def results(request, pergunta_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % pergunta_id)
+def home(request):
+	Lista = Pergunta.objects.order_by('-data')[:5]
+	context = {'Lista': Lista}
+	return render(request, "polls/home.html", context)
+def desenvolvedor(request):
+    return render(request, "polls/desenvolvedor.html", {})
+	
--- mysite/urls.py
+++ mysite/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    url(r'^$', 'blog.views.index', name='index'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
