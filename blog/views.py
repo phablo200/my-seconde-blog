562a74a043f35a9d448eb09fd52a9693f9670092
phablo100 <loermia_iela@hotmail.com>
2015-09-24 12:06:27 -0300

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, Http404
-from django.http import HttpResponse
+from django.shortcuts import render, Http404, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from .models import Pergunta
+from django.core.urlresolvers import reverse
 
 def index(request):
     return render(request, "index.html", {});
@@ -13,10 +14,25 @@ def detail(request, pergunta_id):
         raise Http404("Question does not exist")
     return render(request, 'polls/detail.html', {'pergunta': pergunta})
 def vote(request, pergunta_id):
-	return HttpResponse("Você está votando na pergunta %s" %pergunta_id)	
+    p = get_object_or_404(Pergunta, pk=pergunta_id)
+    try:
+        selected_reposta = p.reposta_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_reposta.votos += 1
+        selected_reposta.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('blog:results', args=(p.id,)))	
 def results(request, pergunta_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % pergunta_id)
+    pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
+    return render(request, 'polls/results.html', {'pergunta': pergunta})
 def home(request):
 	Lista = Pergunta.objects.order_by('-data')[:5]
 	context = {'Lista': Lista}
--- static-in-pro/static-our/Imagens/Phablo Vilas Boas_files/AAqveVBkuTQ.js
+++
@@ -1,5 +0,0 @@
-/*!CK:2277193190!*//*1442950702,*/
-
-if (self.CavalryLogger) { CavalryLogger.start_js(["VlNUN"]); }
-
-__d("VideoChannelLoggerFields",[],function a(b,c,d,e,f,g){c.__markCompiled&&c.__markCompiled();f.exports={ENTRY_POINT:"entry_point",VIDEO_CHAINING_CALLER:"video_chaining_caller",EVENT:"event",POSITION_IN_CHANNEL:"position_in_channel",SESSION_ID:"session_id",STORY_ID:"story_id",TIME_SINCE_START:"time_since_start",VIDEO_CHANNEL_ID:"video_channel_id",VIDEO_CHANNEL_TYPE:"video_channel_type",VIDEO_CHANNEL_TITLE:"video_channel_title",VIDEO_HOME_SESSION_ID:"video_home_session_id"};},null,{});
